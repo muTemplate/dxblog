6a375df52033e14e58efd5c7b6d9859adf501a4a
yojie <1507547774@qq.com>
2020-04-03 14:32:09 +0800

    2020-04-03
    Signed-off-by: yojie <1507547774@qq.com>

---
+++ dxcms/app/admin/common/Base.php
@@ -0,0 +1,15 @@
+<?php
+    
+namespace app\admin\common;
+
+use think\Controller;
+
+class Base extends Controller{
+
+   
+
+
+}
+
+
+
---
+++ dxcms/app/admin/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+
+use app\admin\common\Base;
+
+class Index extends Base
+{
+    public function index()
+    {
+         
+        return  "houtai";
+    } 
+}
